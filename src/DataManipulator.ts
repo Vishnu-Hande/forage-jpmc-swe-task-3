0cdde0c2ed72adf5084ca18242903e0bc31431d8
Vishnu-Hande <Vishnu-Hande>
2024-02-05 16:35:19 +0530

    Create Patch File

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,13 +1,13 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  price_abc:number,
-  price_def:number,
-  ratio:number,
-  upper_bound:number,
-  lower_bound:number,
-  trigger_alert:number|undefined,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
   timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
 }
 
 
@@ -16,7 +16,7 @@ export class DataManipulator {
        const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
        const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
        const ratio = priceABC / priceDEF;
-       const upperBound = 1 +0.05;
+       const upperBound = 1 + 0.05;
        const lowerBound = 1 - 0.05;
        return {
          price_abc: priceABC,
@@ -27,6 +27,6 @@ export class DataManipulator {
          upper_bound: upperBound,
          lower_bound: lowerBound,
          trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
-       };
+       }!;
 }
 }
